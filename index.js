8622a1b2d68908128d7d037b75b61bc2cfcb0717
Jeann <77308262+Jean2232@users.noreply.github.com>
2024-05-15 09:53:07 -0300

    Update index.js
    correção de bugs

--- index.js
+++ index.js
@@ -30,35 +30,48 @@ $('.result-list > div').each((i, e) => {
     const dados = { title, content, diadata, link };
     
     results.push(dados);
+  }})
+    console.log(results)}
 
-    console.log(results)}})}
 autoracingf1()
 
-async function globo_e_fut(){
-  const url = "https://ge.globo.com/futebol/"
-  
-  const response = await axios.get(url);
-  const html = response.data;
-  
-  const $ = cheerio.load(html);
-  
-  const results = [];
-  
-  $('div > ._evg > ._evt > div > ._evg > ._evt').each((i, e) => {
-    const title = $(e).find('h2 > a > p').text();
-    const content = $(e).find('.feed-post-body-resumo > p').text();
-    const post = $(e).find('.feed-post-metadata > .feed-post-datetime').text();
-    const link = $(e).find('h2 > a').attr("href");
-    try{
-      var dateform = post.split(" ")
-      var num_min = dateform[1]
-      var min_h = dateform[2]
-    }catch{var dateform = post}
+async function globo_e_fut() {
+    const url = "https://ge.globo.com/futebol/"
 
-    if (num_min == "9" ||num_min == "8" ||num_min == "7" || num_min == "6" || num_min == "5" || num_min == "4" || num_min == "3" || num_min == "2" || num_min == "1" && min_h == "minutos" ){
-      var dados = { title, content, post, link };
-      results.push(dados);
-      console.log(results)}})}
+    const response = await axios.get(url);
+    const html = response.data;
 
-  
-//globo_e_fut()
+    const $ = cheerio.load(html);
+
+    const results = [];
+
+    $('._evt > div > ._evg > ._evt > div > ._evg > ._evt').each((i, e) => {
+        const title = $(e).find('h2 > a > p').text();
+        const content = $(e).find('.feed-post-body-resumo > p').text();
+        const post = $(e).find('.feed-post-metadata > .feed-post-datetime').text();
+        const link = $(e).find('h2 > a').attr("href");
+        try {
+            var dateform = post.split(" ")
+            var num_min = dateform[1]
+            var min_h = dateform[2]
+        } catch {
+            var dateform = post
+        }
+
+        if (num_min == "9" || num_min == "8" || num_min == "7" || num_min == "6" || num_min == "5" || num_min == "4" || num_min == "3" || num_min == "2" || num_min == "1") {
+            if (min_h == "minutos") {
+                var dados = {
+                    title,
+                    content,
+                    post,
+                    link
+                };
+                results.push(dados)
+            }
+        }
+    })
+    console.log(results)
+}
+
+
+globo_e_fut()
